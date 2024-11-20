191e91d33b983fd2cdce89ba30ba544dbb47e19f
Werianin <makanin.fed@gmail.com>
2024-11-20 13:47:32 +0700

    Update solution

--- mini_06.py
+++ mini_06.py
@@ -1,4 +1,4 @@
-def flat_one_lvl(data:list):
+def flat_one_lvl(data: list):
     result = []
     for l in data:
         if isinstance(l, list):
@@ -6,28 +6,33 @@ def flat_one_lvl(data:list):
                 result.append(j)
         else:
             result.append(l)
-    return result;
+    return result
 
-def flatten(data:list):
+
+def flatten(data: list):
     if all([(isinstance(l, list) == 0) for l in data]):
-        return data;
+        return data
     result = flat_one_lvl(data)
     return flatten(result)
 
-def flatten_depth(data:list, depth:int):
+
+def flatten_depth(data: list, depth: int):
     """
+    >>> flatten([1, 2, [4, 5], [6, [7]], 8]))
+    [1, 2, 4, 5, 6, 7, 8]
     >>> flatten_depth([1, 2, [4, 5], [6, [7]], 8], depth=2)
     [1, 2, 4, 5, 6, 7, 8]
     >>> flatten_depth([1, 2, [4, 5], [6, [7]], 8], depth=1)
     [1, 2, 4, 5, 6, [7], 8]
-    >>> flatten_depth([1, 2, [4, 5], [6, [7]], 8], depth=1488)
+    >>> flatten_depth([1, 2, [4, 5], [6, [7]], 8], depth=10000)
     [1, 2, 4, 5, 6, 7, 8]
     """
-    
+
     if depth == 0 or all([(isinstance(l, list) == 0) for l in data]):
-        return data;
+        return data
     result = flat_one_lvl(data)
     return flatten_depth(result, depth - 1)
+
 
 if __name__ == "__main__":
     import doctest
