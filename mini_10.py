b9747dc0a4a0ab155c1f2a3e8e4b32227afe496d
Werianin <makanin.fed@gmail.com>
2024-11-23 19:09:13 +0700

    Update solution

---
+++ mini_10.py
@@ -0,0 +1,29 @@
+class SingletonMeta(type):
+    instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.instances:
+            cls.instances[cls] = super().__call__(*args, **kwargs)
+        return cls.instances[cls]
+
+
+class SingletonClass(metaclass=SingletonMeta):
+    def __init__(self, value):
+        self.value = value
+
+
+"""
+>>> c1 = SingletonClass("42")
+>>> c2 = SingletonClass("I Hate 42")
+>>> c1 is c2
+True
+>>> c1.value
+42
+>>> c2.value
+42
+"""
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
