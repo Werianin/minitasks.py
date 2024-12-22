2585b8ef3f8174c660f14a883291b0991d7154c3
Werianin <makanin.fed@gmail.com>
2024-12-22 14:10:01 +0700

    Update solution

--- mini_09.py
+++ mini_09.py
@@ -1,27 +1,20 @@
+from collections import OrderedDict
+from functools import wraps
+
+
 class LRUCache:
     def __init__(self, capacity=16):
-        self.cache = {}
-        self.popularity = {}
+        self.cache = OrderedDict()
         self.cap = capacity
-        self.len = 0
 
     def put(self, key, value):
-        if self.len == self.cap:
-            val = max(self.cache.values())
-            k = self.popularity.values().index(val)
-            self.cache.pop(k)
-            self.popularity.pop(k)
-            self.len -= 1
+        if len(self.cache) == self.cap:
+            self.cache.popitem()
         self.cache[key] = value
-        self.popularity[key] = 0
-        self.len += 1
 
     def get(self, key):
         if key in self.cache.keys():
-            self.popularity[key] = 0
-            for k in self.cache.keys():
-                if k != key:
-                    self.popularity[k] += 1
+            self.cache.move_to_end(key, last=False)
             return self.cache[key]
         return None
 
@@ -29,6 +22,7 @@ class LRUCache:
 def memoize(f):
     c = LRUCache(capacity=2)
 
+    @wraps(f)
     def inner(*args, **kwargs):
         key = (args, frozenset(kwargs.items()))
 
@@ -36,10 +30,6 @@ def memoize(f):
             c.put(c, key, f(*args, **kwargs))
 
         return c.get(key)
-
-    inner.__name__ = f.__name__
-    inner.__doc__ = f.__doc__
-    inner.__module__ = f.__module__
 
     return inner
 
